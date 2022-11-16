9452981e0273baa8d7cade7e4e8aac9884958b49
Vu Lien <vuthilien712@gmail.com>
2022-11-16 14:53:11 +0700

    Begin

---
+++ base/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from .models import Task
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello')
+
+class TaskList(ListView): # find task_list.html if not return 
+    model = Task  #listView require a model or queryset
+    context_object_name='tasks'
+    
+class TaskDetail(DetailView): #find task_detail.html
+    model=Task
+    context_object_name='task'
+    template_name='base/task.html'# look for task.html instead of task_detail.html
+
+class TaskCreate(CreateView):
+    model= Task
+    fields='__all__' # list all item of fields
+    success_url=reverse_lazy('tasks')
+
