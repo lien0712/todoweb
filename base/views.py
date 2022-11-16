efc50edcebb7457b67f38f1c317716f963153181
Vu Lien <vuthilien712@gmail.com>
2022-11-16 16:05:21 +0700

    Detele

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView , UpdateView , DeleteView
 from .models import Task
 
 # Create your views here.
@@ -24,3 +24,12 @@ class TaskCreate(CreateView):
     fields='__all__' # list all item of fields
     success_url=reverse_lazy('tasks')
 
+class TaskUpdate(UpdateView):
+    model=Task
+    fields='__all__' 
+    success_url=reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model=Task
+    context_object_name='task'
+    success_url=reverse_lazy('tasks')
